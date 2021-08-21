671c8d254f193adb4033e9f8a2b6d5de200d576b
Viktor Sandberg <sandtain@gmail.com>
2021-08-21 18:16:59 +0200

    update path to files

---
+++ precache-manifest.c373055a175f5eef8423254164428ce1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c96f1a486013badd3683517bb77320bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ef4157c368758fdb245",
+    "url": "/static/css/2.5c5863c1.chunk.css"
+  },
+  {
+    "revision": "9b9e9f8c399a31ab86ff",
+    "url": "/static/css/main.03506ec0.chunk.css"
+  },
+  {
+    "revision": "7ef4157c368758fdb245",
+    "url": "/static/js/2.e20439ae.chunk.js"
+  },
+  {
+    "revision": "95a21561bd181f3e50a06c28ac71b6a6",
+    "url": "/static/js/2.e20439ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b9e9f8c399a31ab86ff",
+    "url": "/static/js/main.b43227e7.chunk.js"
+  },
+  {
+    "revision": "6f0d8640488f50ae70a4",
+    "url": "/static/js/runtime-main.135447cf.js"
+  },
+  {
+    "revision": "b7b04264e9ebeb5db2070261d89ac8ce",
+    "url": "/static/media/aftonbladet.b7b04264.jpg"
+  },
+  {
+    "revision": "417d63e2622d4eb1dc9829e87ceef6df",
+    "url": "/static/media/alarm.417d63e2.mp3"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/build/precache-manifest.cb0fdf19731938b733ceff187f00d43b.js"
+  "/precache-manifest.c373055a175f5eef8423254164428ce1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/build/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
