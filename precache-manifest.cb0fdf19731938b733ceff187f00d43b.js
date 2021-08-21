51d49e4a1bd5b76908242ac2ec6172134bb3a360
Viktor Sandberg <sandtain@gmail.com>
2021-08-21 18:10:41 +0200

    initial commit

---
+++ precache-manifest.cb0fdf19731938b733ceff187f00d43b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e16d5dec0c855324c044a5e6db744167",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "a281bc5a451528b321a9",
+    "url": "/build/static/css/2.5c5863c1.chunk.css"
+  },
+  {
+    "revision": "9b9e9f8c399a31ab86ff",
+    "url": "/build/static/css/main.03506ec0.chunk.css"
+  },
+  {
+    "revision": "a281bc5a451528b321a9",
+    "url": "/build/static/js/2.9d5f30a9.chunk.js"
+  },
+  {
+    "revision": "95a21561bd181f3e50a06c28ac71b6a6",
+    "url": "/build/static/js/2.9d5f30a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b9e9f8c399a31ab86ff",
+    "url": "/build/static/js/main.b43227e7.chunk.js"
+  },
+  {
+    "revision": "14e5edf2779ca91593f1",
+    "url": "/build/static/js/runtime-main.d3f1eeb0.js"
+  },
+  {
+    "revision": "b7b04264e9ebeb5db2070261d89ac8ce",
+    "url": "/build/static/media/aftonbladet.b7b04264.jpg"
+  },
+  {
+    "revision": "417d63e2622d4eb1dc9829e87ceef6df",
+    "url": "/build/static/media/alarm.417d63e2.mp3"
+  }
+]);
