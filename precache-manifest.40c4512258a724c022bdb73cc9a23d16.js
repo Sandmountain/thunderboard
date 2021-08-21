39e3166a06157666908465fc8a7bd11b1c36f073
Viktor Sandberg <sandtain@gmail.com>
2021-08-22 01:30:37 +0200

    change homepage url

---
+++ precache-manifest.40c4512258a724c022bdb73cc9a23d16.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ee3149f0593cec9477f3b04a15cab73",
+    "url": "/thunderboard/index.html"
+  },
+  {
+    "revision": "3d99ba47804e5fa1d109",
+    "url": "/thunderboard/static/css/2.5c5863c1.chunk.css"
+  },
+  {
+    "revision": "f6ff7d7a91abde3e7f31",
+    "url": "/thunderboard/static/css/main.03506ec0.chunk.css"
+  },
+  {
+    "revision": "3d99ba47804e5fa1d109",
+    "url": "/thunderboard/static/js/2.b7f96621.chunk.js"
+  },
+  {
+    "revision": "95a21561bd181f3e50a06c28ac71b6a6",
+    "url": "/thunderboard/static/js/2.b7f96621.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6ff7d7a91abde3e7f31",
+    "url": "/thunderboard/static/js/main.dadfc0b4.chunk.js"
+  },
+  {
+    "revision": "15177af0685437970930",
+    "url": "/thunderboard/static/js/runtime-main.4620fa7d.js"
+  },
+  {
+    "revision": "b7b04264e9ebeb5db2070261d89ac8ce",
+    "url": "/thunderboard/static/media/aftonbladet.b7b04264.jpg"
+  },
+  {
+    "revision": "417d63e2622d4eb1dc9829e87ceef6df",
+    "url": "/thunderboard/static/media/alarm.417d63e2.mp3"
+  }
+]);
